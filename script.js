3acbac8315a72564db7c13da43ea3a22362edc8d
Farhan <74499472+fb0801@users.noreply.github.com>
2021-09-20 18:50:11 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const cards = document.querySelectorAll('.memory-card');
+let hasFlippedCard = false;
+let firstCard = secondCard;
+
+
 
 function flipCard(){
-    this.classlist.toggle('flip');
+    this.classlist.add('flip');
+    if(!hasFlippedCard){
+        //first card click
+        hasFlippedCard = true;
+        firstCard = this;
+
+    } else {
+        //second click
+        hasFlippedCard = false;
+        secondCard = this;
+
+        //check if card match
+        
+    }
 }
 
 
